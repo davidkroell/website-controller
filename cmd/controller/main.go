6d3039f79f07759eb8b1dac17937e3c9058360ea
David Kröll <david.kroell@outlook.com>
2025-08-28 12:06:57 +0200

    fix names and refactor to internal package

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -7,13 +7,9 @@ import (
 	wsapiv1 "website-operator/api/v1"
 	"website-operator/internal"
 
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -38,10 +34,6 @@ type reconciler struct {
 	kubeClient *kubernetes.Clientset
 }
 
-const (
-	nginxPort = 80
-)
-
 func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	siteName := "website-" + req.Name
 	log := log.FromContext(ctx).WithValues("siteName", siteName)
@@ -55,51 +47,51 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	err := r.Client.Get(ctx, req.NamespacedName, &website)
 	if err != nil && k8serrors.IsNotFound(err) {
 		// website not found -> delete it
-		err = deploymentsClient.Delete(ctx, siteName, metav1.DeleteOptions{})
+		err = deploymentsClient.Delete(ctx, internal.DeploymentObjectName(siteName), metav1.DeleteOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't delete deployment: %s", err)
 		}
-		err = cmClient.Delete(ctx, siteName, metav1.DeleteOptions{})
+		err = cmClient.Delete(ctx, internal.ConfigMapObjectName(siteName), metav1.DeleteOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't delete configmap: %s", err)
 		}
-		err = svcClient.Delete(ctx, siteName, metav1.DeleteOptions{})
+		err = svcClient.Delete(ctx, internal.ServiceObjectName(siteName), metav1.DeleteOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't delete service: %s", err)
 		}
-		err = ingressClient.Delete(ctx, siteName, metav1.DeleteOptions{})
+		err = ingressClient.Delete(ctx, internal.IngressObjectName(siteName), metav1.DeleteOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't delete ingress: %s", err)
 		}
 		return ctrl.Result{}, err
 	}
 
-	deployment, err := deploymentsClient.Get(ctx, siteName, metav1.GetOptions{})
+	deployment, err := deploymentsClient.Get(ctx, internal.DeploymentObjectName(siteName), metav1.GetOptions{})
 	if err != nil && k8serrors.IsNotFound(err) {
 		// create new website object
-		cmObj := getConfigMapObject(siteName, website.Spec.HtmlContent)
+		cmObj := internal.CreateConfigMapObject(siteName, website.Spec)
 		_, err = cmClient.Create(ctx, cmObj, metav1.CreateOptions{})
 		if err != nil && !k8serrors.IsAlreadyExists(err) {
 			return ctrl.Result{}, fmt.Errorf("couldn't create configmap: %s", err)
 		}
 
-		deploymentObj := getDeploymentObject(siteName, website.Spec)
+		deploymentObj := internal.CreateDeploymentObject(siteName, website.Spec)
 		_, err := deploymentsClient.Create(ctx, deploymentObj, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create deployment: %s", err)
 		}
 
-		log.Info("new website created")
+		log.Info("new deployment created for website")
 
-		svcObject := getServiceObject(siteName)
+		svcObject := internal.CreateServiceObject(siteName)
 		svcObject, err = svcClient.Create(ctx, svcObject, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create service: %s", err)
 		}
 
-		log.Info("new service created for website, exposed now via nodePort")
+		log.Info("new service created for website")
 
-		ingressObject := getIngressObj(siteName, website.Spec.Hostname)
+		ingressObject := internal.CreateIngressObj(siteName, website.Spec)
 		ingressObject, err = ingressClient.Create(ctx, ingressObject, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create ingress: %s", err)
@@ -124,7 +116,7 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	}
 
 	// look up hostname change
-	ingressSpec, err := ingressClient.Get(ctx, siteName, metav1.GetOptions{})
+	ingressSpec, err := ingressClient.Get(ctx, internal.IngressObjectName(siteName), metav1.GetOptions{})
 	if err == nil {
 		if ingressSpec.Spec.Rules[0].Host != website.Spec.Hostname {
 			log.Info("ingress spec hostname need update")
@@ -140,7 +132,7 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	}
 
 	// look up config map differences in HTML contents
-	confMap, err := cmClient.Get(ctx, siteName, metav1.GetOptions{})
+	confMap, err := cmClient.Get(ctx, internal.ConfigMapObjectName(siteName), metav1.GetOptions{})
 	if err == nil &&
 		confMap.Data["index.html"] != website.Spec.HtmlContent {
 		// config map differs, update required
@@ -157,141 +149,6 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 	log.Info("website is up-to-date")
 
 	return ctrl.Result{}, nil
-}
-
-func ptr[T any](v T) *T {
-	return &v
-}
-
-func getIngressObj(name, hostname string) *networkingv1.Ingress {
-	const ingressClassName = "nginx"
-
-	return &networkingv1.Ingress{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: networkingv1.IngressSpec{
-			IngressClassName: ptr(ingressClassName),
-			Rules: []networkingv1.IngressRule{
-				{
-					Host: hostname,
-					IngressRuleValue: networkingv1.IngressRuleValue{
-						HTTP: &networkingv1.HTTPIngressRuleValue{
-							Paths: []networkingv1.HTTPIngressPath{
-								{
-									Path:     "/",
-									PathType: ptr(networkingv1.PathTypePrefix),
-									Backend: networkingv1.IngressBackend{
-										Service: &networkingv1.IngressServiceBackend{
-											Name: name,
-											Port: networkingv1.ServiceBackendPort{
-												Number: nginxPort,
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func getServiceObject(name string) *corev1.Service {
-	return &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Spec: corev1.ServiceSpec{
-			Selector: map[string]string{
-				"apptype":           "website",
-				"anexia.com/expose": name,
-			},
-			Type: corev1.ServiceTypeNodePort,
-			Ports: []corev1.ServicePort{
-				{
-					Name:     "http",
-					Protocol: "TCP",
-					Port:     nginxPort,
-					TargetPort: intstr.IntOrString{
-						IntVal: nginxPort,
-					},
-				},
-			},
-		},
-	}
-}
-
-func getDeploymentObject(name string, spec wsapiv1.WebSiteSpec) *appsv1.Deployment {
-	return &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{Name: name},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: ptr(int32(1)), // TODO replicas
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"apptype": "website",
-				},
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"apptype":           "website",
-						"anexia.com/expose": name,
-					},
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "website",
-							Image: spec.NginxImage,
-							Ports: []corev1.ContainerPort{
-								{
-									Name:          "http-svc-port",
-									Protocol:      corev1.ProtocolTCP,
-									ContainerPort: nginxPort,
-								},
-							},
-							VolumeMounts: []corev1.VolumeMount{
-								{
-									Name:      "contents",
-									MountPath: "/usr/share/nginx/html",
-								},
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "contents",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: name,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func getConfigMapObject(name, contents string) *corev1.ConfigMap {
-	return &corev1.ConfigMap{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "ConfigMap",
-			APIVersion: "v1",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Data: map[string]string{
-			"index.html": contents,
-		},
-	}
 }
 
 func main() {
---
+++ internal/kube_objects.go
@@ -0,0 +1,167 @@
+package internal
+
+import (
+	wsapiv1 "website-operator/api/v1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	netv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+const (
+	nginxPort = 80
+
+	ingressPath      = "/"
+	ingressClassName = "nginx"
+
+	websiteReplica = 1
+)
+
+func IngressObjectName(siteName string) string {
+	return siteName + "-ingress"
+}
+
+func ServiceObjectName(siteName string) string {
+	return siteName + "-service"
+}
+
+func DeploymentObjectName(siteName string) string {
+	return siteName + "-deploy"
+}
+
+func ConfigMapObjectName(siteName string) string {
+	return siteName + "-cm"
+}
+
+func CreateIngressObj(name string, spec wsapiv1.WebSiteSpec) *netv1.Ingress {
+	return &netv1.Ingress{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: IngressObjectName(name),
+		},
+		Spec: netv1.IngressSpec{
+			IngressClassName: Ptr(ingressClassName),
+			Rules: []netv1.IngressRule{
+				{
+					Host: spec.Hostname,
+					IngressRuleValue: netv1.IngressRuleValue{
+						HTTP: &netv1.HTTPIngressRuleValue{
+							Paths: []netv1.HTTPIngressPath{
+								{
+									Path:     ingressPath,
+									PathType: Ptr(netv1.PathTypePrefix),
+									Backend: netv1.IngressBackend{
+										Service: &netv1.IngressServiceBackend{
+											Name: ServiceObjectName(name),
+											Port: netv1.ServiceBackendPort{
+												Number: nginxPort,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func CreateServiceObject(name string) *corev1.Service {
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ServiceObjectName(name),
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: map[string]string{
+				"apptype":           "website",
+				"anexia.com/expose": DeploymentObjectName(name),
+			},
+			Type: corev1.ServiceTypeNodePort,
+			Ports: []corev1.ServicePort{
+				{
+					Name:     "http",
+					Protocol: "TCP",
+					Port:     nginxPort,
+					TargetPort: intstr.IntOrString{
+						IntVal: nginxPort,
+					},
+				},
+			},
+		},
+	}
+}
+
+func CreateDeploymentObject(name string, spec wsapiv1.WebSiteSpec) *appsv1.Deployment {
+	return &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: DeploymentObjectName(name),
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: Ptr(int32(websiteReplica)),
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"apptype": "website",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"apptype":           "website",
+						"anexia.com/expose": DeploymentObjectName(name),
+					},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "website",
+							Image: spec.NginxImage,
+							Ports: []corev1.ContainerPort{
+								{
+									Name:          "http-svc-port",
+									Protocol:      corev1.ProtocolTCP,
+									ContainerPort: nginxPort,
+								},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "contents",
+									MountPath: "/usr/share/nginx/html",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "contents",
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: ConfigMapObjectName(name),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func CreateConfigMapObject(name string, spec wsapiv1.WebSiteSpec) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "ConfigMap",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ConfigMapObjectName(name),
+		},
+		Data: map[string]string{
+			"index.html": spec.HtmlContent,
+		},
+	}
+}
