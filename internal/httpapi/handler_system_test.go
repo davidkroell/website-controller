cf7fc0ad3f44b7895498077a3d92262243cc29fb
David Kröll <david.kroell@outlook.com>
2025-08-29 10:01:14 +0200

    add full lifecycle of website

--- internal/httpapi/handler_system_test.go
+++ internal/httpapi/handler_system_test.go
@@ -1,160 +1,85 @@
 package httpapi
 
 import (
-	"bufio"
 	"context"
-	"crypto/rand"
-	"fmt"
-	"io"
-	"net"
 	"net/http"
-	"os"
-	"os/exec"
 	"testing"
 	"time"
 	"website-operator/httpapiclient"
 	"website-operator/internal"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-var apiBinaryPath = internal.FromEnvWithDefault("TEST_API_BINARY_PATH", "../../bin/httpapi")
+var apiClient *httpapiclient.Client
 
-func RunSystemTestStandaloneAPI(t *testing.T, testFunc func(client *httpapiclient.Client, t *testing.T)) {
+func TestMain(m *testing.M) {
 	baseURL := internal.FromEnvWithDefault("TEST_API_BASEURL", "http://localhost:8082/")
 
-	client, err := httpapiclient.NewDefaultClient(baseURL)
-	require.NoError(t, err)
-
-	// run testFunc
-	testFunc(client, t)
-}
-
-func RunSystemTest(t *testing.T, testFunc func(client *httpapiclient.Client, t *testing.T)) {
-	// let the OS choose a port at random
-	l, err := net.Listen("tcp", "localhost:0")
-	require.NoError(t, err)
-	port := l.Addr().(*net.TCPAddr).Port
-	l.Close()
-
-	baseURL := fmt.Sprintf("http://localhost:%d/", port)
-	client, err := httpapiclient.NewDefaultClient(baseURL)
-	require.NoError(t, err)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	cmd := exec.CommandContext(ctx, apiBinaryPath)
-	cmd.Env = append(os.Environ(), fmt.Sprintf("HTTPAPI_LISTEN_ADDR=:%d", port))
-
-	stdout, err := cmd.StdoutPipe()
-	require.NoError(t, err)
-
-	stderr, err := cmd.StderrPipe()
-	require.NoError(t, err)
-
-	err = cmd.Start()
-	require.NoError(t, err)
-
-	// Goroutine to stream stdout
-	go streamOutput(ctx, t, stdout, "T_STDOUT")
-	// Goroutine to stream stderr
-	go streamOutput(ctx, t, stderr, "T_STDERR")
-
-	// run testFunc
-	testFunc(client, t)
-
-	// Kill the process when test is done
-	err = cmd.Process.Kill()
-	require.NoError(t, err)
-	_ = cmd.Wait()
-}
-
-// helper: copy lines to t.Log
-func streamOutput(ctx context.Context, t *testing.T, r io.ReadCloser, name string) {
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			t.Logf("[%s] %s", name, scanner.Text())
-		}
+	var err error
+	apiClient, err = httpapiclient.NewDefaultClient(baseURL)
+	if err != nil {
+		panic(err)
 	}
-	if err := scanner.Err(); err != nil {
-		t.Logf("error reading %s: %v", name, err)
-	}
+
+	m.Run()
 }
 
-func TestSystem_All(t *testing.T) {
-	RunSystemTestStandaloneAPI(t, func(client *httpapiclient.Client, t *testing.T) {
-		sitename := genRandom("website")
-		const hostname = "test.minikube.local"
-		const url = "http://" + hostname
-		htmlContents := genRandom("html-body-content")
+func TestSystem_CreateListUpdateDeleteWebsite(t *testing.T) {
+	sitename := genRandom("website")
+	const hostname = "test.minikube.local"
+	const nginx128 = "docker.io/nginx:1.28"
+	const nginx129 = "docker.io/nginx:1.29"
+	htmlContents := genRandom("html-body-content")
 
-		_, err := client.CreateWebsite(context.Background(), httpapiclient.WebsiteCreateDTO{
-			WebsiteBase: httpapiclient.WebsiteBase{
-				HtmlContent: htmlContents,
-				Hostname:    hostname,
-				NginxImage:  "docker.io/nginx:1.28",
-			},
-			Name: sitename,
-		})
-		assert.NoError(t, err)
-
-		assert.NoError(t, WaitUntilAvailable(url, 30*time.Second))
-
-		resp, err := http.Get(url)
-		assert.NoError(t, err)
-		b, err := io.ReadAll(resp.Body)
-		assert.NoError(t, err)
-		contents := string(b)
-		assert.Equal(t, htmlContents, contents)
+	_, err := apiClient.CreateWebsite(context.Background(), httpapiclient.WebsiteCreateDTO{
+		WebsiteBase: httpapiclient.WebsiteBase{
+			HtmlContent: htmlContents,
+			Hostname:    hostname,
+			NginxImage:  nginx128,
+		},
+		Name: sitename,
 	})
-}
+	defer apiClient.DeleteWebsite(context.Background(), sitename) // make sure website is deleted always (in case of test fails)
+	assert.NoError(t, err)
 
-func WaitUntilAvailable(url string, d time.Duration) error {
-	backoff := 500 * time.Millisecond
-	deadline := time.Now().Add(d)
+	resp, err := waitUntilHttpStatus(hostname, 30*time.Second, http.StatusOK)
+	assert.NoError(t, err)
+	assertResponseHasContent(t, err, resp, htmlContents)
 
-	for {
-		// Try request
-		resp, err := http.Get(url)
-		if err == nil && resp.StatusCode == http.StatusOK {
-			_ = resp.Body.Close()
-			return nil // success
-		}
-		if resp != nil {
-			_ = resp.Body.Close()
-		}
+	// LIST websites
 
-		// Check timeout
-		if time.Now().After(deadline) {
-			return fmt.Errorf("service at %s not available within %v", url, d)
-		}
+	websites, err := apiClient.ListWebsites(context.Background())
+	site := getWebsiteByName(websites, sitename)
+	assert.Equal(t, htmlContents, site.HtmlContent)
+	assert.Equal(t, hostname, site.Hostname)
+	assert.Equal(t, nginx128, site.NginxImage)
+	assert.Equal(t, int64(1), site.Generation)
+	assert.WithinDuration(t, time.Now(), site.CreationTimestamp, time.Minute)
 
-		// Sleep with exponential backoff, capped at 5s
-		time.Sleep(backoff)
-		backoff *= 2
-		if backoff > 5*time.Second {
-			backoff = 5 * time.Second
-		}
-	}
-}
+	// UPDATE website
+	const newHostname = "a.minikube.local"
+	updatedHtmlContent := htmlContents + "-updated-from-test"
+	updatedWebsite, err := apiClient.UpdateWebsite(context.Background(), sitename, httpapiclient.WebsiteUpdateDTO{
+		WebsiteBase: httpapiclient.WebsiteBase{
+			HtmlContent: updatedHtmlContent,
+			Hostname:    newHostname,
+			NginxImage:  nginx129,
+		},
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, newHostname, updatedWebsite.Hostname)
+	assert.Equal(t, nginx129, updatedWebsite.NginxImage)
+	assert.Equal(t, updatedHtmlContent, updatedWebsite.HtmlContent)
 
-const letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+	resp, err = waitUntilHttpStatus(newHostname, 30*time.Second, http.StatusOK)
+	assert.NoError(t, err)
+	assertResponseHasContent(t, err, resp, updatedHtmlContent)
 
-func genRandom(prefix string) string {
-	const n = 8
+	// DELETE website
+	err = apiClient.DeleteWebsite(context.Background(), sitename)
+	assert.NoError(t, err)
 
-	b := make([]byte, n)
-	if _, err := rand.Read(b); err != nil {
-		panic(fmt.Sprintf("failed to read random bytes: %v", err))
-	}
-	for i := 0; i < n; i++ {
-		b[i] = letters[int(b[i])%len(letters)]
-	}
-	return "ci-" + prefix + "-" + string(b)
+	_, err = waitUntilHttpStatus(newHostname, 30*time.Second, http.StatusNotFound)
+	assert.NoError(t, err)
 }
---
+++ internal/httpapi/util_test.go
@@ -0,0 +1,79 @@
+package httpapi
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+	"website-operator/httpapiclient"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func genRandom(prefix string) string {
+	const letters = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+	const n = 8
+
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		panic(fmt.Sprintf("failed to read random bytes: %v", err))
+	}
+	for i := 0; i < n; i++ {
+		b[i] = letters[int(b[i])%len(letters)]
+	}
+	return "ci-" + prefix + "-" + string(b)
+}
+
+func waitUntilHttpStatus(url string, d time.Duration, status int) (*http.Response, error) {
+	return waitUntilHttpFulfills(url, d, func(resp *http.Response) bool {
+		return resp.StatusCode == status
+	})
+}
+
+func waitUntilHttpFulfills(url string, d time.Duration, predicate func(resp *http.Response) bool) (*http.Response, error) {
+	if !strings.HasPrefix(url, "http://") || !strings.HasPrefix(url, "https://") {
+		url = "http://" + url
+	}
+	backoff := 500 * time.Millisecond
+	deadline := time.Now().Add(d)
+
+	for {
+		// Try request
+		resp, err := http.Get(url)
+		if err == nil && predicate(resp) {
+			return resp, nil // fulfills predicate
+		}
+
+		// Check timeout
+		if time.Now().After(deadline) {
+			return nil, fmt.Errorf("service at %s not available within %v", url, d)
+		}
+
+		// Sleep with exponential backoff, capped at 5s
+		time.Sleep(backoff)
+		backoff *= 2
+		if backoff > 5*time.Second {
+			backoff = 5 * time.Second
+		}
+	}
+}
+
+func assertResponseHasContent(t *testing.T, err error, resp *http.Response, content string) {
+	b, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	respContent := string(b)
+	assert.Equal(t, content, respContent)
+}
+
+func getWebsiteByName(s httpapiclient.WebsiteListDTO, name string) *httpapiclient.WebsiteDTO {
+	for _, w := range s {
+		if w.Name == name {
+			return w
+		}
+	}
+	return nil
+}
