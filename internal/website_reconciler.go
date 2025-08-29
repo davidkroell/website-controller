46960e6ed4eac5540824a146cf0a529a33421777
David Kröll <david.kroell@outlook.com>
2025-08-29 11:12:29 +0200

    refactor website finalize

--- internal/website_reconciler.go
+++ internal/website_reconciler.go
@@ -3,10 +3,10 @@ package internal
 import (
 	"context"
 	"fmt"
-	"website-operator/api/v1"
+	wsapiv1 "website-operator/api/v1"
 
 	"k8s.io/apimachinery/pkg/api/errors"
-	v2 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -38,40 +38,22 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	svcClient := r.kubeClient.CoreV1().Services(req.Namespace)
 	ingressClient := r.kubeClient.NetworkingV1().Ingresses(req.Namespace)
 
-	var website v1.WebSite
-	err := r.Client.Get(ctx, req.NamespacedName, &website)
-	if err != nil && errors.IsNotFound(err) {
-		// website not found -> delete it
-		err = deploymentsClient.Delete(ctx, DeploymentObjectName(siteName), v2.DeleteOptions{})
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("couldn't delete deployment: %s", err)
-		}
-		err = cmClient.Delete(ctx, ConfigMapObjectName(siteName), v2.DeleteOptions{})
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("couldn't delete configmap: %s", err)
-		}
-		err = svcClient.Delete(ctx, ServiceObjectName(siteName), v2.DeleteOptions{})
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("couldn't delete service: %s", err)
-		}
-		err = ingressClient.Delete(ctx, IngressObjectName(siteName), v2.DeleteOptions{})
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("couldn't delete ingress: %s", err)
-		}
-		return ctrl.Result{}, err
+	website, err := r.getWebsite(ctx, req)
+	if r.needsFinalizeWebsite(err) {
+		return r.finalizeWebsite(ctx, req, siteName)
 	}
 
-	deployment, err := deploymentsClient.Get(ctx, DeploymentObjectName(siteName), v2.GetOptions{})
+	deployment, err := deploymentsClient.Get(ctx, DeploymentObjectName(siteName), metav1.GetOptions{})
 	if err != nil && errors.IsNotFound(err) {
 		// create new website object
 		cmObj := CreateConfigMapObject(siteName, website.Spec)
-		_, err = cmClient.Create(ctx, cmObj, v2.CreateOptions{})
+		_, err = cmClient.Create(ctx, cmObj, metav1.CreateOptions{})
 		if err != nil && !errors.IsAlreadyExists(err) {
 			return ctrl.Result{}, fmt.Errorf("couldn't create configmap: %s", err)
 		}
 
 		deploymentObj := CreateDeploymentObject(siteName, website.Spec)
-		_, err := deploymentsClient.Create(ctx, deploymentObj, v2.CreateOptions{})
+		_, err := deploymentsClient.Create(ctx, deploymentObj, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create deployment: %s", err)
 		}
@@ -79,7 +61,7 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		log.Info("new deployment created for website")
 
 		svcObject := CreateServiceObject(siteName)
-		svcObject, err = svcClient.Create(ctx, svcObject, v2.CreateOptions{})
+		svcObject, err = svcClient.Create(ctx, svcObject, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create service: %s", err)
 		}
@@ -87,7 +69,7 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		log.Info("new service created for website")
 
 		ingressObject := CreateIngressObj(siteName, website.Spec)
-		ingressObject, err = ingressClient.Create(ctx, ingressObject, v2.CreateOptions{})
+		ingressObject, err = ingressClient.Create(ctx, ingressObject, metav1.CreateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't create ingress: %s", err)
 		}
@@ -102,7 +84,7 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		// update deployment
 		deployment.Spec.Template.Spec.Containers[0].Image = website.Spec.NginxImage
 
-		deployment, err = deploymentsClient.Update(ctx, deployment, v2.UpdateOptions{})
+		deployment, err = deploymentsClient.Update(ctx, deployment, metav1.UpdateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't update deployment: %s", err)
 		}
@@ -111,13 +93,13 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	}
 
 	// look up hostname change
-	ingressSpec, err := ingressClient.Get(ctx, IngressObjectName(siteName), v2.GetOptions{})
+	ingressSpec, err := ingressClient.Get(ctx, IngressObjectName(siteName), metav1.GetOptions{})
 	if err == nil {
 		if ingressSpec.Spec.Rules[0].Host != website.Spec.Hostname {
 			log.Info("ingress spec hostname need update")
 			ingressSpec.Spec.Rules[0].Host = website.Spec.Hostname
 
-			_, err = ingressClient.Update(ctx, ingressSpec, v2.UpdateOptions{})
+			_, err = ingressClient.Update(ctx, ingressSpec, metav1.UpdateOptions{})
 			if err != nil {
 				return ctrl.Result{}, fmt.Errorf("couldn't update ingress spec: %s", err)
 			}
@@ -127,14 +109,14 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	}
 
 	// look up config map differences in HTML contents
-	confMap, err := cmClient.Get(ctx, ConfigMapObjectName(siteName), v2.GetOptions{})
+	confMap, err := cmClient.Get(ctx, ConfigMapObjectName(siteName), metav1.GetOptions{})
 	if err == nil &&
 		confMap.Data["index.html"] != website.Spec.HtmlContent {
 		// config map differs, update required
 
 		confMap.Data["index.html"] = website.Spec.HtmlContent
 
-		_, err = cmClient.Update(ctx, confMap, v2.UpdateOptions{})
+		_, err = cmClient.Update(ctx, confMap, metav1.UpdateOptions{})
 		if err != nil {
 			return ctrl.Result{}, fmt.Errorf("couldn't update ConfigMap: %s", err)
 		}
@@ -144,4 +126,39 @@ func (r *WebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 	log.Info("website is up-to-date")
 
 	return ctrl.Result{}, nil
+}
+
+func (r *WebsiteReconciler) getWebsite(ctx context.Context, req ctrl.Request) (*wsapiv1.WebSite, error) {
+	var website wsapiv1.WebSite
+	err := r.Client.Get(ctx, req.NamespacedName, &website)
+	return &website, err
+}
+
+func (r *WebsiteReconciler) needsFinalizeWebsite(err error) bool {
+	return err != nil && errors.IsNotFound(err)
+}
+
+func (r *WebsiteReconciler) finalizeWebsite(ctx context.Context, req ctrl.Request, siteName string) (ctrl.Result, error) {
+	deploymentsClient := r.kubeClient.AppsV1().Deployments(req.Namespace)
+	cmClient := r.kubeClient.CoreV1().ConfigMaps(req.Namespace)
+	svcClient := r.kubeClient.CoreV1().Services(req.Namespace)
+	ingressClient := r.kubeClient.NetworkingV1().Ingresses(req.Namespace)
+
+	err := deploymentsClient.Delete(ctx, DeploymentObjectName(siteName), metav1.DeleteOptions{})
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("couldn't delete deployment: %s", err)
+	}
+	err = cmClient.Delete(ctx, ConfigMapObjectName(siteName), metav1.DeleteOptions{})
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("couldn't delete configmap: %s", err)
+	}
+	err = svcClient.Delete(ctx, ServiceObjectName(siteName), metav1.DeleteOptions{})
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("couldn't delete service: %s", err)
+	}
+	err = ingressClient.Delete(ctx, IngressObjectName(siteName), metav1.DeleteOptions{})
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("couldn't delete ingress: %s", err)
+	}
+	return ctrl.Result{}, err
 }
